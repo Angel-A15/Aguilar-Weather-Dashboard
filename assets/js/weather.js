135fb2c296cdef94d43ca5c2b9dd9d64176fb507
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2022-08-20 18:00:47 -0500

    started on functions for the js

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,9 +1,40 @@
-var weatherAPI =function(){
-    var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={966a05338762aed9f430379fd9d68bf9}";
+// var apiKey = 966a05338762aed9f430379fd9d68bf9;
+var fetchBtn = document.querySelector('fetchBtn')
+var statsContainer = document.getElementById('stats');
+
+
+// //function for current searched city
+// var crntCityWeather = function(){
+//     //request the github api URL
+//     var apiUrl = "https://api.openweathermap.org/data/3.0/" + "onecall?lat={lat}&lon={lon}&direction=desc&appid={apiKy}";
+
+//     //make a get request to url
+//     fetch(apiUrl).then(function(response){
+
+//         if(response.ok) {
+
+//             response.json().then(function(data){
+//                 displayIssues(data);
+//             })
+//         }
+//     })
+// }
+
+function getApi(){
+    var requestUrl = "https://api.openweathermap.org/data/3.0/" + "onecall?lat={lat}&lon={lon}&direction=desc&appid={apiKy}";
 
     fetch(requestUrl)
         .then(function(response){
             return response.json();
         })
 
+    .then(function(data){
+        //will display to check for response
+        console.log(data);
+
+    });
+
 }
+
+//fetch search city data/havent made getApi function
+fetchBtn.addEventListener('click', getApi); 
