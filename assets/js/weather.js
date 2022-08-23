92a3361ecbd17cf74c57d4eb2489def5307756fe
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2022-08-23 08:30:52 -0500

    changed the structure of the functions that fetch apis

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -16,50 +16,73 @@ function searchBtn(event) {
   //will fetch the information the user is looking for
   var cityVal = searchValue.value;
 
-  //calling weather api and passing the city name for the api call
-  getWeatherApi(cityVal);
+  //this was replaced
+  // getFiveDayApi(cityVal);
 
-  getFiveDayApi(cityVal);
+  getLocationApi(cityVal);
 
 
 }
 
 
 //function for current forecast
-function getWeatherApi(cityName) {
+function getWeatherApi(location) {
+  //function getWeatherApi(cityName) {
+
+  //this was added 
+  var lat = location.lat;
+  var lon = location.lon;
+  console.log(lat, lon);
   fetch(
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-      cityName +
-      "&appid=966a05338762aed9f430379fd9d68bf9"
-  )
+    'https://api.openweathermap.org/data/3.0/onecall?lat=' +lat+ '&lon=' +lon+ '&exclude=hourly,daily&appid=e163b24ee160aaebaf6d65a0eb3d960b'
+  )   
+  //  "https://api.openweathermap.org/data/2.5/weather?q=" +
+  // cityName +
+  // "&appid=966a05338762aed9f430379fd9d68bf9"
+
     .then(function (response) {
+ 
       return response.json();
     })
     .then(function (data) {
       //will display to check for response
-    
-      today.innerHTML = `
-      <p id="city-name">City: ${data.name}</p>
-      <p id="temperature">Temperature: ${data.main.temp} F</p>
-      <p id="wind">Wind: ${data.wind.speed} MPH</p>
-      <p id="humidity">Humdity: ${data.main.humidity} %</p>
-      <p id="uv">UV Index: ${data.uvi}</p>`
-    
+      console.log(data);
+      function getWeatherApi(cityName) {
+
+      
+      // <p id="city-name">City: ${data.name}</p>
+      // <p id="temperature">Temperature: ${data.main.temp} F</p>
+      // <p id="wind">Wind: ${data.wind.speed} MPH</p>
+      // <p id="humidity">Humdity: ${data.main.humidity} %</p>
+      // <p id="uv">UV Index: ${data.uvi}</p>`
+      }
 
     });
 }
 
 //function for 5-day forecast
-function getFiveDayApi(cityName){
+function getLocationApi(cityName){
+  // function getFiveDayApi(cityName){
+
   fetch(
-    'https://api.openweathermap.org/data/2.5/?=' +cityName +
-    '&units=imperial&appid=966a05338762aed9f430379fd9d68bf9'
-  )
+    'http://api.openweathermap.org/geo/1.0/direct?q='+cityName+ '&limit=5&appid=e163b24ee160aaebaf6d65a0eb3d960b'
+  )    
+  // 'https://api.openweathermap.org/data/2.5/?=' +cityName +
+  // '&units=imperial&appid=966a05338762aed9f430379fd9d68bf9'
+
+
   .then(function(response){
+    console.log(response);
     return response.json();
+    
   })
   .then(function(data){
     console.log(data);
+
+    //this was added
+    getWeatherApi(data[0])
+    
+    //this was in testing for five day weather
     // today.innerHTML = `
     // <p id="city-name">City: ${data.name}</p>
     // <p id="temperature">Temperature: ${data.main.temp} F</p>
