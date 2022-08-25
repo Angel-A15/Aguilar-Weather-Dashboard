2fcbfe9e74838fc323e8c40c3f6c3d834f726551
Angel-A15 <106582411+Angel-A15@users.noreply.github.com>
2022-08-24 22:54:22 -0500

    established ground on collecting the 5-days forecast data

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -4,7 +4,7 @@ var searchValue = document.querySelector("#search-bar");
 
 //weather condition variables
 // var cityNameVal = document.querySelector("#city-name");
-
+var fiveDays = document.querySelector("#future")
 var today = document.querySelector("#todaysWeather")
 var listedCities = document.querySelector("#searched-cities")
 
@@ -24,21 +24,15 @@ function searchBtn(event) {
 
 }
 
-
 //function for current forecast
-function getWeatherApi(location) {
-  //function getWeatherApi(cityName) {
+function getLocationApi(cityName) {
 
-  //this was added 
-  var lat = location.lat;
-  var lon = location.lon;
-  console.log(lat, lon);
   fetch(
-    'https://api.openweathermap.org/data/3.0/onecall?lat=' +lat+ '&lon=' +lon+ '&exclude=hourly,daily&appid=e163b24ee160aaebaf6d65a0eb3d960b'
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    cityName +
+    "&appid=966a05338762aed9f430379fd9d68bf9"
   )   
-  //  "https://api.openweathermap.org/data/2.5/weather?q=" +
-  // cityName +
-  // "&appid=966a05338762aed9f430379fd9d68bf9"
+
 
     .then(function (response) {
  
@@ -47,52 +41,53 @@ function getWeatherApi(location) {
     .then(function (data) {
       //will display to check for response
       console.log(data);
-      function getWeatherApi(cityName) {
-
+      //displays recieved data
+      today.innerHTML = `
+      <p id="city-name">City: ${data.name}</p>
+      <p id="temperature">Temperature: ${data.main.temp} K</p>
+      <p id="wind">Wind: ${data.wind.speed} MPH</p>
+      <p id="humidity">Humdity: ${data.main.humidity} %</p>
+      <p id="uv">UV Index: ${data.uvi}</p>`
+    
+      getWeatherApi(data)
       
-      // <p id="city-name">City: ${data.name}</p>
-      // <p id="temperature">Temperature: ${data.main.temp} F</p>
-      // <p id="wind">Wind: ${data.wind.speed} MPH</p>
-      // <p id="humidity">Humdity: ${data.main.humidity} %</p>
-      // <p id="uv">UV Index: ${data.uvi}</p>`
-      }
 
     });
 }
 
-//function for 5-day forecast
-function getLocationApi(cityName){
-  // function getFiveDayApi(cityName){
 
+// //function for current forecast
+function getWeatherApi(location) {
+  //function getWeatherApi(cityName) {
+
+  //this was added 
+  var lat = location.coord.lat;
+  var lon = location.coord.lon;
+  console.log(lat, lon);
   fetch(
-    'http://api.openweathermap.org/geo/1.0/direct?q='+cityName+ '&limit=5&appid=e163b24ee160aaebaf6d65a0eb3d960b'
-  )    
-  // 'https://api.openweathermap.org/data/2.5/?=' +cityName +
-  // '&units=imperial&appid=966a05338762aed9f430379fd9d68bf9'
+    'https://api.openweathermap.org/data/2.5/forecast?lat=' +lat+ '&lon=' +lon+ '&exclude=hourly,daily&appid=966a05338762aed9f430379fd9d68bf9'
+  )   
 
+    .then(function (response) {
+ 
+      return response.json();
+    })
+    .then(function (data) {
+      //will display to check for response
+      console.log(data);
+      fiveDays.inner=`
+      <p id="day-1">Day 1: ${data.name}</p>`
 
-  .then(function(response){
-    console.log(response);
-    return response.json();
-    
-  })
-  .then(function(data){
-    console.log(data);
+      console.log(new Date(data.list[0].dt*1000))
+      console.log(new Date(data.list[8].dt*1000))
+      console.log(new Date(data.list[16].dt*1000))
+      console.log(new Date(data.list[24].dt*1000))
+      console.log(new Date(data.list[32].dt*1000))
 
-    //this was added
-    getWeatherApi(data[0])
-    
-    //this was in testing for five day weather
-    // today.innerHTML = `
-    // <p id="city-name">City: ${data.name}</p>
-    // <p id="temperature">Temperature: ${data.main.temp} F</p>
-    // <p id="wind">Wind: ${data.wind.speed} MPH</p>
-    // <p id="humidity">Humdity: ${data.main.humidity} %</p>
-    // <p id="uv">UV Index: ${data.uvi}</p>`
-  })
+      }
+
+    )
 }
-
-
 
 //local storage function
 function weatherStorage(city) {
@@ -112,9 +107,6 @@ function weatherStorage(city) {
 
 }
 
-
-//5 day api
-//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 //fetch search city data
 fetchBtn.addEventListener("click", searchBtn);
